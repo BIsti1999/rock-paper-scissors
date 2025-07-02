7ffa717fb6a15660b92de44750fd30666e582275
BIsti1999 <ibaranyai1999@gmail.com>
2025-07-02 20:01:31 +0200

    Add user interface to the game

--- javascript.js
+++ javascript.js
@@ -9,75 +9,97 @@ function getComputerChoice() {
     }
     } 
 
-function getHumanChoice() {
-    let humanChoice = prompt("Rock, paper or scissors?");
-    if (humanChoice.toLowerCase() === "rock") {
-        return "rock";
-    }   else if (humanChoice.toLowerCase() === "paper") {
-        return "paper";
-    }   else if (humanChoice.toLowerCase() === "scissors") {
-        return "scissors";
-        }
-    } 
-
 function playRound(computerChoice, humanChoice) {
     if (computerChoice === "rock") {
         if (humanChoice === "rock") {
-            console.log("Rock = rock, draw!");
+            result.textContent = "Draw! Rock = rock!";
+            container.insertBefore(result, finalResult);
             return "";
         }
         else if (humanChoice === "paper") {
-            console.log("Paper beats rock, you won!");
+            result.textContent = "You won! Paper beats rock!";
+            container.insertBefore(result, finalResult);
             return "human"; 
         }
         else if (humanChoice === "scissors") {
-            console.log("Rock beats scissors, you lost!");
+            result.textContent = "You lost! Rock beats scissors!";
+            container.insertBefore(result, finalResult);
             return "computer"; 
         }
     }
     if (computerChoice === "paper") {
         if (humanChoice === "rock") {
-            console.log("Paper beats rock, you lost!");
+            result.textContent = "You lost! Paper beats rock!";
+            container.insertBefore(result, finalResult);
             return "computer";
         }
         else if (humanChoice === "paper") {
-            console.log("Paper = paper, draw!");
+            result.textContent = "Draw! Paper = paper!";
+            container.insertBefore(result, finalResult);
             return "";    
         }
         else if (humanChoice === "scissors") {
-            console.log("Scissors beats paper, you won!");
+            result.textContent = "You won! Scissors beats paper!";
+            container.insertBefore(result, finalResult);
             return "human";
         }
     }
     if (computerChoice === "scissors") {
         if (humanChoice === "rock") {
-            console.log("Rock beats scissors, you won!");
+            result.textContent = "You won! Rock beats scissors!";
+            container.insertBefore(result, finalResult);
             return "human";
         }
         else if (humanChoice === "paper") {
-            console.log("Scissors beats paper, you lost!");
+            result.textContent = "You lost! Scissors beats paper!";
+            container.insertBefore(result, finalResult);
             return "computer";
         }
         else if (humanChoice === "scissors") {
-            console.log("Scissors = scissors, draw!");
+            result.textContent = "Draw! Scissors = scissors!";
+            container.insertBefore(result, finalResult);
             return "";
         }
     }
 } 
 
-function playGame() {
-    let computerScore = 0;
-    let humanScore = 0;
-    let winner = ""; 
-    for (let i = 1; i <=5; i++) {
-        winner = playRound(getComputerChoice(), getHumanChoice());
-        if (winner === "computer") {
-            computerScore++;
-        }   else if (winner === "human") {
-            humanScore++;
-        }
-    }
-    console.log("The result is: Computer " + computerScore + ":" + humanScore + " Human.");
-}
+let humanScore = 0;
+let computerScore = 0;
+const container = document.querySelector("div");
+const finalResult = document.createElement("div");
+finalResult.textContent = "***";
+container.appendChild(finalResult);
+const result = document.createElement("div");
+result.textContent = "Waiting for your choice!";
+container.insertBefore(result, finalResult);
+const content = document.createElement("div");
+content.textContent = "HUMAN " + humanScore + ":" + computerScore + " COMPUTER";
+container.insertBefore(content, result);
 
-playGame();
+
+let winner = "";
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        winner = (playRound(getComputerChoice(), button.id));
+        if (winner === "human") {
+            content.textContent = "HUMAN " + ++humanScore + ":" + computerScore + " COMPUTER";
+            container.insertBefore(content, result);
+        } else if (winner === "computer") {
+            content.textContent = "HUMAN " + humanScore + ":" + ++computerScore + " COMPUTER";
+            container.insertBefore(content, result);
+        }
+        if (humanScore === 5) {
+            finalResult.textContent = "You won the previous game! " + humanScore + ":" + computerScore;
+            container.appendChild(finalResult);
+            humanScore = 0;
+            computerScore = 0;
+        } else if (computerScore === 5) {
+            finalResult.textContent = "You lost the previous game! " + computerScore + ":" + humanScore;
+            container.appendChild(finalResult);
+            humanScore = 0;
+            computerScore = 0;
+        };
+        })
+    });
+
